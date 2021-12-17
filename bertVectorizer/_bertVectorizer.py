2f6e5e97510e7d98b3045db5a606ab97c29fb2d6
Luiz Martins <luizmartins.uemg@gmail.com>
2021-12-17 12:36:03 -0300

    Created new structure for Vectorizer

--- bertVectorizer/_bertVectorizer.py
+++ bertVectorizer/_bertVectorizer.py
@@ -3,255 +3,62 @@
 #          Luiz Henrique Dutra Martins <luizmartins.uemg@gmail.com>
 
 import pandas as pd
-import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
-from keybert import KeyBERT
-from typing import List, Union, Tuple
-from tqdm import tqdm
+import nltk 
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
 
 __all__ = ['bertVectorizer']
 
 
 class bertVectorizer():
-    r"""Convert a collection of raw documents to a Pandas DataFrame extracted from BERT resources. 
-
-    The bertVectorizer uses CountVectorizer to build a list of keyword candidates and uses KeyBERT to identify the similarity between each candidate and the given sentence. We recommend that you read the documentation for both. 
-
-    Parameters
-    ----------
-
-    stemmer : bool, default=False
-        Performs stemming in the text 
-
-    lowercase : bool, default=True
-        Convert all characters to lowercase before tokenizing
-
-    ngram_range : tuple (min_n, max_n), default=(1, 1)
-        The lower and upper boundary of the range of n-values for different
-        word n-grams or char n-grams to be extracted. All values of n such
-        such that min_n <= n <= max_n will be used. For example an
-        ``ngram_range`` of ``(1, 1)`` means only unigrams, ``(1, 2)`` means
-        unigrams and bigrams, and ``(2, 2)`` means only bigrams.
-        Only applies if ``analyzer is not callable``.
-
-    stop_words : {'english'}, list, default=None
-        If 'english', a built-in stop word list for English is used.
-        There are several known issues with 'english' and you should
-        consider an alternative (see :ref:`stop_words`).
-
-        If a list, that list is assumed to contain stop words, all of which
-        will be removed from the resulting tokens.
-        Only applies if ``analyzer == 'word'``.
-
-        If None, no stop words will be used. max_df can be set to a value
-        in the range [0.7, 1.0) to automatically detect and filter stop
-        words based on intra corpus document frequency of terms.
-
-    max_df : float in range [0.0, 1.0] or int, default=1.0
-        When building the vocabulary ignore terms that have a document
-        frequency strictly higher than the given threshold (corpus-specific
-        stop words).
-        If float, the parameter represents a proportion of documents, integer
-        absolute counts.
-        This parameter is ignored if vocabulary is not None.
-
-    min_df : float in range [0.0, 1.0] or int, default=1
-        When building the vocabulary ignore terms that have a document
-        frequency strictly lower than the given threshold. This value is also
-        called cut-off in the literature.
-        If float, the parameter represents a proportion of documents, integer
-        absolute counts.
-        This parameter is ignored if vocabulary is not None.
-
-    max_features : int, default=None
-        If not None, build a vocabulary that only consider the top
-        max_features ordered by term frequency across the corpus.
-
-        This parameter is ignored if vocabulary is not None.
-
-    vocabulary : Mapping or iterable, default=None
-        Either a Mapping (e.g., a dict) where keys are terms and values are
-        indices in the feature matrix, or an iterable over terms. If not
-        given, a vocabulary is determined from the input documents. Indices
-        in the mapping should not be repeated and should not have any gap
-        between 0 and the largest index.
-
-    kbert_candidates : List, default=None 
-        Candidate keywords/keyphrases to use instead of extracting them from the document(s)
-
-    kbert_model : String, default='distilbert-base-nli-mean-tokens.
-        Select a BERT model
-
-    kbert_top_n : int, default=5
-        Return the top n keywords/keyphrases
-
-    kbert_min_df : int, default=1
-        Minimum document frequency of a word across all documents if keywords for multiple documents need to be extracted
-
-    kbert_use_maxsum : bool, default=False
-        Whether to use Max Sum Similarity for the selection of keywords/keyphrases
-
-    kbert_use_mmr : bool, default=False
-        Whether to use Maximal Marginal Relevance (MMR) for the selection of keywords/keyphrases
-
-    kbert_diversity : float, default=0.5
-        The diversity of the results between 0 and 1 if use_mmr is set to True
-
-    kbert_nr_candidates : int, default=20
-        The number of candidates to consider if use_maxsum is set to True
-
-    kbert_vectorizer : CountVectorizer, default=None
-        Pass in your own CountVectorizer from scikit-learn
-
-    kbert_highlight : bool, default=False
-        Whether to print the document and highlight its keywords/keyphrases. NOTE: This does not work if multiple documents are passed. 
-
-    kbert_seed_keywords : list, default=None
-        Seed keywords that may guide the extraction of keywords by steering the similarities towards the seeded keywords
+    r"""
 
     """
-
     def __init__(self,
-                 stemmer: bool = False,
-                 lowercase: bool = True,
-                 ngram_range: Tuple = (1, 1),
-                 stop_words=None,
-                 max_df: float = 1.0,
-                 min_df: float = 1,
-                 max_features:  int = None,
-                 vocabulary: Union[str, List[str]] = None,
-                 kbert_candidates: List[str] = None,
-                 kbert_model: str = 'distilbert-base-nli-mean-tokens',
-                 kbert_top_n: int = 5,
-                 kbert_min_df: int = 1,
-                 kbert_use_maxsum: bool = False,
-                 kbert_use_mmr: bool = False,
-                 kbert_diversity: float = 0.5,
-                 kbert_nr_candidates: int = 20,
-                 kbert_vectorizer: CountVectorizer = None,
-                 kbert_highlight: bool = False,
-                 kbert_seed_keywords: List[str] = None,
-                 normalize: bool = True) -> None:
+                bert_model='nli-distilroberta-base-v2') -> None:
+        self.bert_model = bert_model
 
-        self.stemmer = stemmer
-        self.lowercase = lowercase
-        self.ngram_range = ngram_range
-        self.stop_words = stop_words
-        self.max_df = max_df
-        self.min_df = min_df
-        self.max_features = max_features
-        self.vocabulary = vocabulary
-        self.kbert_candidates = kbert_candidates
-        self.kbert_top_n = kbert_top_n
-        self.kbert_min_df = kbert_min_df
-        self.kbert_use_maxsum = kbert_use_maxsum
-        self.kbert_use_mmr = kbert_use_mmr
-        self.kbert_diversity = kbert_diversity
-        self.kbert_nr_candidates = kbert_nr_candidates
-        self.kbert_vectorizer = kbert_vectorizer
-        self.kbert_highlight = kbert_highlight
-        self.kbert_seed_keywords = kbert_seed_keywords
-        self.normalize = normalize
-
-        # CREATE THE KEYBERT MODEL
-        self.kbert_model = KeyBERT(model=kbert_model)
-
-    def features_from_cv(self, data):
-        """[Use CountVectorizer to build a list of feature names]
+    def encode_data(self, data, candidates):
+        """[summary]
 
         Args:
-            data ([list]): [A list of texts]
+            data ([type]): [description]
+            candidates ([type]): [description]
 
         Returns:
-            [list]: [Return a list of feature names]
+            [type]: [description]
         """
-        cv_vectorizer = CountVectorizer(lowercase=self.lowercase,
-                                        ngram_range=self.ngram_range,
-                                        stop_words=self.stop_words,
-                                        max_df=self.max_df,
-                                        min_df=self.min_df,
-                                        max_features=self.max_features,
-                                        vocabulary=self.vocabulary)
+        model = SentenceTransformer(self.bert_model)
 
-        cv_vectorizer.fit_transform(data)
+        emb_data = model.encode(data)
+        emb_candidates = model.encode(candidates)
 
-        return cv_vectorizer.get_feature_names()
+        return emb_data, emb_candidates
 
-    def features_keybert(self, text):
-        """[Use keybert to build a list of feature names]
+    def get_features(self, data):
+        """[summary]
 
         Args:
-            text ([list]): [A list of texts]
+            data ([type]): [description]
 
         Returns:
-            [type]: [Return a list of feature names]
+            [type]: [description]
         """
-        kw_model = self.kbert_model
-        keys = list()
-        result = 0
+        candidates = []
 
-        for txt, prog in zip(text, tqdm(range(0, len(text)))):
-            result += prog
-            if txt == 'no texts':
-                continue
-
-            keywords = kw_model.extract_keywords(txt,
-                                                 candidates=self.kbert_candidates,
-                                                 keyphrase_ngram_range=self.ngram_range,
-                                                 stop_words=self.stop_words,
-                                                 top_n=self.kbert_top_n,
-                                                 min_df=self.kbert_min_df,
-                                                 use_maxsum=self.kbert_use_maxsum,
-                                                 use_mmr=self.kbert_use_mmr,
-                                                 diversity=self.kbert_diversity,
-                                                 nr_candidates=self.kbert_nr_candidates,
-                                                 vectorizer=self.kbert_vectorizer,
-                                                 highlight=self.kbert_highlight,
-                                                 seed_keywords=self.kbert_seed_keywords)
-
-            keys.append(keywords)
-            # print(keywords)
-
-        vocab = []
-        for k in keys:
-            for (kw, vl) in k:
-                vocab.append(kw)
-
-        vocabulary = set(d for d in vocab)
-
-        print(vocabulary)
-
-        return list(vocabulary)
+        return candidates
 
     def fit_transform(self, data):
         """[summary]
 
         Args:
-            data ([list]): [A list of texts]
+            data ([type]): [description]
 
         Returns:
-            [Pandas DataFrame]: [return a dataframe of values representing the similarity between a candidate and the respective text]
+            [Pandas DataFrame]: [description]
         """
-        if self.vocabulary == 'keybert':
-            self.vocabulary = self.features_keybert(data)
-        else:
-            self.vocabulary = self.features_from_cv(data)
+        matrix = []
 
-        keybert_model = self.kbert_model
-        weights_bert = []
+        candidates = self.get_features(data)
 
-        for item in data:
-
-            keybert_result = sorted(keybert_model.extract_keywords(
-                item, candidates=self.vocabulary, top_n=len(self.vocabulary)))  # extract similarity
-
-            # create list of values representing the similarity of each word to text
-            items = []
-            for item in keybert_result:
-                items.append(item[1])
-
-            weights_bert.append(items)
-
-        df_bert = pd.DataFrame(columns=self.vocabulary, data=weights_bert)
-
-        return df_bert
+        return pd.DataFrame(columns=candidates, data=matrix)
